26d0ec473d0d5299a442dcc638dec220989c190e
ssalvi8586 <ssalvi8586@gmail.com>
2021-08-19 14:20:34 +0600

    header w/ and w/o profile and log-reg button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,169 @@
-import { Link } from "react-router-dom";
+import { AppBar, Box, Grid, IconButton, Toolbar, Paper, TextField, Typography, Button, makeStyles, createTheme, ThemeProvider, MenuItem } from '@material-ui/core';
+import * as React from 'react';
 
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import { blue, green } from '@material-ui/core/colors';
+import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
+import { Link } from 'react-router-dom';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import Menu from '@material-ui/core/Menu';
+
+
+
+
+
+const useStyles = makeStyles(() => ({
+
+
+
+    toolbar: {
+        textAlign: "center",
+
+
+    },
+    btn: {
+        textAlign: "center",
+        margin: "1%",
+        marginTop: "3%",
+
+
+    },
+
+
+}));
+
+const theme = createTheme({
+    palette: {
+        primary: green,
+        secondary: blue,
+    },
+});
 
 const Header = () => {
+
+
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleProfile = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+
+    const classes = useStyles();
+
     return (
-        <>
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Grid container spacing={3} alignItems='center' justifyContent='center' flexGrow='1'>
+                        <Grid item xs>
+                            <TextField style={{ width: '50%' }} type="search" variant="outlined" placeholder='Search' />
+                            <IconButton><SearchIcon /></IconButton>
+                        </Grid>
+                        <Grid item xs={5} lg={8}>
 
-            <div className="container">
-                <header className="blog-header pb-3">
-                    <div className="row flex-nowrap justify-content-between align-items-center">
-                        <div className="col-4 pt-1 d-flex">
-                            <img src="https://img.icons8.com/material-outlined/40/000000/menu--v1.png" />
-                            <form className="d-flex" action="{{route('home')}}" method="post">
-                                <input className="w-100 form-control mx-4" type="text" name="search" id="search" placeholder="search" />
-                                <input className="btn btn-primary rounded-3" type="submit" defaultValue="Search" />
-                            </form>
-                        </div>
-                        <div className="col-4 text-center">
-                            <a className="blog-header-logo text-dark" href="{{ route('home') }}">Student Portal</a>
-                        </div>
-                        <div className="col-4 d-flex justify-content-end align-items-center">
-                            <Link className="btn btn-outline-success me-3" to="/register">Register</Link>
-                            <Link className="btn btn-outline-primary me-3" to="/login">Login</Link>
-                            
-                        </div>
-                    </div>
-                </header>
+                            <Typography variant="h4" component="div">Student Potal</Typography>
 
-            </div>
+                        </Grid>
+                        <Grid item xs>
 
-        </>
+
+                            {/* if not logged in then this part from*/}
+
+
+                            {/* <ThemeProvider theme={theme}>
+                                <Link to="/login" style={{ textDecoration: 'none' }}>
+                                    <Button variant="outlined" color="secondary" className={classes.btn}>
+                                        Login
+                                    </Button>
+                                </Link>
+                                <Link to="/register" style={{ textDecoration: 'none' }}>
+                                    <Button variant="outlined" color="primary" className={classes.btn}>
+                                        Register
+                                    </Button>
+                                </Link>
+                            </ThemeProvider> */}
+
+
+                            {/* if not logged in then this part to this*/}
+
+
+                            {/* if logged in then this part from this*/}
+
+
+
+                            <Grid container spacing={0} alignItems='center' justifyContent='center'>
+                                <Grid item xs>
+                                    <IconButton >
+                                        <NotificationsNoneOutlinedIcon />
+                                    </IconButton>
+                                </Grid>
+                                <div>
+                                    <IconButton aria-haspopup="true" onClick={handleProfile}>
+                                        <PersonOutlineOutlinedIcon />
+
+                                    </IconButton>
+                                    <Menu
+                                        id="simple-menu"
+                                        anchorEl={anchorEl}
+                                        keepMounted
+                                        open={Boolean(anchorEl)}
+                                        onClose={handleClose}
+                                    >
+                                        <Link to="/profile/view" style={{ textDecoration: 'none' }}>
+                                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                        </Link>
+                                        <Link to="#" style={{ textDecoration: 'none' }}>
+                                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                                        </Link>
+
+
+                                    </Menu>
+                                    <div>
+                                        <Typography variant="body2">
+                                            username
+                                        </Typography>
+                                    </div>
+                                    <div>
+                                        <Typography variant="caption">
+                                            usertype
+                                        </Typography>
+                                    </div>
+                                </div>
+
+                            </Grid>
+
+                            {/* if logged in then this part to this*/}
+
+
+                        </Grid>
+
+
+
+                    </Grid>
+
+
+                </Toolbar>
+                <hr />
+            </Box>
+
+        </div >
+
     );
 };
 export default Header;
